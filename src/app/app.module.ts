7b4dd0d9dc34dc97136a8928e23869c0b3d459ba
Hunter <hgall7@gmail.com>
2024-06-04 19:00:16 -0400

    Updated tutorial website with my name and home icon

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [
+        MatIconModule, MatToolbarModule
+    ]
+})
+class AppModule { }
